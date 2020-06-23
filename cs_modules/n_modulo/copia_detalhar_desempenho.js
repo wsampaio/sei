b00f0d701a12f6163c52092be1e2d4857ff400fb
wsampaio <well7cordas@yahoo.com.br>
2020-06-23 16:38:05 -0300

    ADD copiar dados da pagina detalhar_desempenho

---
+++ cs_modules/n_modulo/copia_detalhar_desempenho.js
@@ -0,0 +1,72 @@
+
+var dados = "";
+var correPaginas = false;
+
+$("#divInfraBarraLocalizacao").append(
+"<hr><textarea id='areaDeCopia'></textarea><hr>" +
+"<button id='copiarTudo' style='display:none;'>Copiar Tudo</button>"
+);
+
+$("#copiarTudo").on("click", function(){
+	navigator.clipboard.writeText("funcCopiar" + dados);
+	document.getElementById("lnkInfraProximaPaginaSuperior").click();
+});
+
+navigator.clipboard.readText().then(text => {
+
+	dados = text;
+
+	if ($("#selInfraPaginacaoSuperior").val() == 0){
+		navigator.clipboard.writeText("");
+		$("#areaDeCopia").append(
+			"Processo;" +
+			"Tipo;" +
+			"Abertura;" +
+			"Conclusão\n"
+		);
+		$("#copiarTudo").css("display", "block");
+	} else {
+		if (text.substr(0,10) == "funcCopiar"){
+			correPaginas = true;
+			$("#areaDeCopia").append(text.substr(10));
+			dados = text.substr(10);
+		}
+	}
+
+	for (let obj of $(".protocoloNormal")) {
+		$("#areaDeCopia").append(
+			$(obj).html() + ";" +
+			$(obj).attr("title") + ";" +
+			$($(obj).parent().siblings()[1]).html() + ";" +
+			$($(obj).parent().siblings()[2]).html() + "" +
+			"\n" 
+		);
+	}
+
+	var copyText = document.getElementById("areaDeCopia");
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 999999999); /*For mobile devices*/
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+	dados = copyText.value;
+	navigator.clipboard.writeText(dados);
+	//alert("texto copiado\n" + copyText.value)
+
+	if($("#lnkInfraProximaPaginaSuperior").length == 0){
+		correPaginas = false;
+		alert("dados copiados");
+	}
+
+
+
+	if (correPaginas){
+		navigator.clipboard.writeText("funcCopiar" + dados);
+		document.getElementById("lnkInfraProximaPaginaSuperior").click();
+	}
+
+	//$("#copiarTudo").css("display", "block");
+	
+	
+});
+
